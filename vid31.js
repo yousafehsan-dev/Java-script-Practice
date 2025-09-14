a99c0849fad44c5004e07b0eca7588b9abe48190
yousafehsan-dev <yousafehsan125@gmail.com>
2025-09-14 14:01:26 +0500

    addfile

---
+++ vid31.js
@@ -0,0 +1,14 @@
+let age =prompt("enter your age")
+age = Number.parseInt(age)
+
+const canDrive = (age)=>{
+    return age>=18?true:false 
+}
+if(canDrive (age)){
+    alert("yes you can drive")
+}
+else {
+    alert("yes you can drive")
+}
+
+let array =[2,5]
